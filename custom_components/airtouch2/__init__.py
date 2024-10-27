35be2ff789b5a74d26c628415fca4c3927793c9a
Amit Keret <amitkeretwork@gmail.com>
2024-10-27 20:11:50 +1030

    Update __init__.py

--- custom_components/airtouch2/__init__.py
+++ custom_components/airtouch2/__init__.py
@@ -42,6 +42,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+    await hass.config_entries.async_reload(entry.entry_id)
